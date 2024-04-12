6880e31e912b37ce66fa80cd33f0a1c54cabacbe
Luca Rusu <lucarusu0303@gmail.com>
2024-04-12 22:35:42 +0300

    tema completa

--- src/main/java/ro/ulbs/tema/Contact.java
+++ src/main/java/ro/ulbs/tema/Contact.java
@@ -1,2 +1,36 @@
-package ro.ulbs.tema;public class Contact {
+package ro.ulbs.tema;
+
+public class Contact {
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String phoneType;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String phoneType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.phoneType = phoneType;
+    }
+
+    public void printContact(){
+        System.out.println(this.firstName + " " + this.lastName + " " + this.phoneNumber
+        + " " + this.phoneType);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneType() {
+        return phoneType;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + " " + phoneNumber + " " + phoneType;
+    }
 }
--- src/main/java/ro/ulbs/tema/Main.java
+++ src/main/java/ro/ulbs/tema/Main.java
@@ -1,2 +1,59 @@
-package ro.ulbs.tema;public class Main {
+package ro.ulbs.tema;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        p1();
+    }
+
+    static void p1(){
+        List<Contact> contacts = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("input_tema.txt"))) {
+            while (scanner.hasNextLine()) {
+                String[] parts = scanner.nextLine().split(" ");
+                Contact contact = new Contact(parts[0], parts[1], parts[2], parts[3]);
+                contacts.add(contact);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        for (Contact c : contacts) {
+            c.printContact();
+        }
+        System.out.println();
+        Collections.sort(contacts, Comparator.comparing(Contact::getFirstName));
+        Collections.sort(contacts, Comparator.comparing(Contact::getLastName));
+        Collections.sort(contacts, Comparator.comparing(Contact::getPhoneType));
+        for (Contact c : contacts) {
+            c.printContact();
+        }
+
+        Map<String, Integer> phoneCounts = new HashMap<>();
+        for (Contact contact : contacts) {
+            phoneCounts.put(contact.phoneNumber, phoneCounts.getOrDefault(contact.phoneNumber, 0) + 1);
+        }
+
+        try{
+            FileOutputStream fos = new FileOutputStream("output_tema.txt");
+            OutputStreamWriter sw = new OutputStreamWriter(fos);
+            BufferedWriter writer = new BufferedWriter(sw);
+            for (Contact contact : contacts) {
+                String line = contact.toString();
+                if (phoneCounts.get(contact.phoneNumber) >= 2) {
+                    line += " *";
+                }
+                writer.write(line);
+                writer.write(System.lineSeparator());
+                System.out.println("s-a scris");
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
